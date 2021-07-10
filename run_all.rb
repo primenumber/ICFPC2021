c5fe9f76f6a080ad9ba05bc5259f6aa17b9653e6
primenumber <prime@kmc.gr.jp>
2021-07-10 11:39:55 +0900

    Implement score, Improve run_solve

--- run_all.rb
+++ run_all.rb
@@ -5,6 +5,8 @@ $ENDPOINT = 'https://poses.live'
 $TOKEN = File::open('TOKEN.txt').read
 $PATH = "target/release/icfpc2021"
 
+$RANGE = 1..78
+
 def post_answer(id)
   data = File::open("best/#{id}.out").read
   url = URI.parse("#{$ENDPOINT}/api/problems/#{id}/solutions")
@@ -15,27 +17,49 @@ def post_answer(id)
 end
 
 def post_all
-  (1..59).each {|id|
+  $RANGE.each {|id|
     puts id
     puts "ID= #{post_answer(id)}"
   }
 end
 
-def run_solve_impl(i)
+def run_solve_impl(i, quiet=false)
   score = `#{$PATH} solve -p problems/#{i}.in -a answer/#{i}.out`.to_i
-  puts "##{i}: #{score}"
+  if !quiet then
+    puts "##{i}: #{score}"
+  end
   old_score = `#{$PATH} score -p problems/#{i}.in -a best/#{i}.out`.to_i
   if score < old_score then
-    puts "Updated! #{i}"
+    puts "Updated! #{i}, Score=#{score}"
     `cp answer/#{i}.out best/#{i}.out`
-    #post_answer(i)
+    true
+  else
+    false
   end
-  score
 end
 
 def run_solve
-  results = Parallel.map(1..59) {|i|
-    run_solve_impl(i)
+  Parallel.map($RANGE) {|i|
+    updated = false
+    start = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+    loop {
+      updated_new = run_solve_impl(i, true)
+      updated = updated || updated_new
+      now = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+      if now - start > 180 then  # 3min
+        break
+      end
+      best_score = `#{$PATH} score -p problems/#{i}.in -a best/#{i}.out`.to_i
+      if best_score == 0 then
+        break
+      end
+    }
+    [updated, i]
+  }.each {|b,idx|
+    if b
+      puts idx
+      post_answer(idx)
+    end
   }
 end
 
@@ -46,4 +70,8 @@ when "solve" then
   run_solve_impl(ARGV[1].to_i)
 when "post" then
   puts post_answer(ARGV[1].to_i)
+when "score" then
+  $RANGE.each {|i|
+    puts i, `#{$PATH} score -p problems/#{i}.in -a best/#{i}.out`
+  }
 end
